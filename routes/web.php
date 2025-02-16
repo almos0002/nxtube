affaf2c068e56a3cedd5354da6d1dbc7deb4a75c
Almos Mali <almos0002@gmail.com>
2025-02-16 13:45:25 +0545

    routes auth

--- routes/web.php
+++ routes/web.php
@@ -5,48 +5,39 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ActorController;
 use App\Http\Controllers\ChannelController;
 use App\Http\Controllers\VideoController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('admin.dashboard');
-})->name('dashboard');
-Route::get('/videos', function () {
-    return view('admin.video');
-})->name('videos');
-Route::get('/actors', function () {
-    return view('admin.actor');
-})->name('actors');
-Route::get('/categories', function () {
-    return view('admin.category');
-})->name('categories');
-Route::get('/channels', function () {
-    return view('admin.channel');
-})->name('channels');
-Route::get('/settings', function () {
-    return view('admin.setting');
-})->name('settings');
-Route::get('/profile', function () {
-    return view('admin.profile');
-})->name('profile');
-Route::get('/add-video', function () {
-    return view('crud.video.add');
-})->name('add-video');
-
-Route::get('/add-category', [CategoryController::class, 'create'])->name('add-category');
-Route::post('/store-category', [CategoryController::class, 'store'])->name('store-category');
-
-Route::get('/add-actor', [ActorController::class, 'create'])->name('add-actor');
-Route::post('/store-actor', [ActorController::class, 'store'])->name('store-actor');
-
-Route::get('/add-channel', [ChannelController::class, 'create'])->name('add-channel');
-Route::post('/store-channel', [ChannelController::class, 'store'])->name('store-channel');
-
-Route::get('/add-video', [VideoController::class, 'create'])->name('add-video');
-Route::post('/store-video', [VideoController::class, 'store'])->name('store-video');
-
-
 Auth::routes();
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+    // Admin Dashboard & Pages
+    Route::view('/dashboard', 'admin.dashboard')->name('dashboard');
+    Route::view('/videos', 'admin.video')->name('videos');
+    Route::view('/actors', 'admin.actor')->name('actors');
+    Route::view('/categories', 'admin.category')->name('categories');
+    Route::view('/channels', 'admin.channel')->name('channels');
+    Route::view('/settings', 'admin.setting')->name('settings');
+    Route::view('/profile', 'admin.profile')->name('profile');
+
+    // Category Routes
+    Route::get('/add-category', [CategoryController::class, 'create'])->name('add-category');
+    Route::post('/store-category', [CategoryController::class, 'store'])->name('store-category');
+
+    // Actor Routes
+    Route::get('/add-actor', [ActorController::class, 'create'])->name('add-actor');
+    Route::post('/store-actor', [ActorController::class, 'store'])->name('store-actor');
+
+    // Channel Routes
+    Route::get('/add-channel', [ChannelController::class, 'create'])->name('add-channel');
+    Route::post('/store-channel', [ChannelController::class, 'store'])->name('store-channel');
+
+    // Video Routes
+    Route::get('/add-video', [VideoController::class, 'create'])->name('add-video');
+    Route::post('/store-video', [VideoController::class, 'store'])->name('store-video');
+});
